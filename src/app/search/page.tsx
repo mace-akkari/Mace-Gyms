81c51d5f7ce0e3dacb1eae3b147d55ceb32a06ed
Mace Akkari <mace.akkari@relyfe.com>
2024-05-14 21:28:33 +0100

    add search submitHandler

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,26 @@
+'use client'
+
+import { FormEvent } from "react";
+
 export default function Search() {
+
+const submitHandler = (e:FormEvent) => {
+  e.preventDefault();
+  console.log("hello")
+}
+
   return (
     <>
     <h2>Search</h2>
+    <form onSubmit={submitHandler}>
+      <input type="search" placeholder="Search" className="border border-sky-500"></input>
+      <button>Search</button>
+    </form>
+    <output>output:</output>
     </>
   );
 }
+
+// colour scheme
+// display fake api and apply styling 
+// route handler in Next.js to display jason
