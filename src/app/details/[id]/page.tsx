4d20cde24690b2bbb1ed8e1549d47ca1cae6537e
Mace Akkari <mace.akkari@relyfe.com>
2024-06-11 22:41:40 +0100

    add new params

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 type PageArgs = { params: { id: string } };
 
 export default function Page({ params }: PageArgs) {
-  return <h2>Details : {params.id}</h2>;
+  const [details, setDetails] = useState({ license_author: null });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`/api/details/${params.id}`);
+      const json = await res.json();
+      setDetails(json);
+    };
+    fetchData();
+  }, []);
+
+  return <h2>Details : {details?.license_author ?? "Loading..."}</h2>;
 }
+
+//extract - const res = await fetch(`/api/details/${params.id}`);
+//make it into a function-  const json = await res.json();
