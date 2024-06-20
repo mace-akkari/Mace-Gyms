e07d18a59a5681e9e68aee6f95ef9b3b218d4df1
Mace Akkari <mace.akkari@relyfe.com>
2024-06-20 19:08:09 +0100

    add ExerciseDetails page

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,23 +1,34 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import ExerciseInfo from "./ExerciseInfo";
+import { PageProps } from "./interfaces";
 
-type PageArgs = { params: { id: string } };
+const ExerciseDetails = ({ params }: PageProps) => {
+  return <ExerciseInfo params={params} />;
+};
 
-export default function Page({ params }: PageArgs) {
-  const [details, setDetails] = useState({ license_author: null });
+export default ExerciseDetails;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(`/api/details/${params.id}`);
-      const json = await res.json();
-      setDetails(json);
-    };
-    fetchData();
-  }, []);
+// "use client";
 
-  return <h2>Details : {details?.license_author ?? "Loading..."}</h2>;
-}
+// import { useEffect, useState } from "react";
+
+// type PageArgs = { params: { id: string } };
+
+// export default function Page({ params }: PageArgs) {
+//   const [details, setDetails] = useState({ license_author: null });
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const res = await fetch(`/api/details/${params.id}`);
+//       const json = await res.json();
+//       setDetails(json);
+//     };
+//     fetchData();
+//   }, []);
+
+//   return <h2>Details : {details?.license_author ?? "Loading..."}</h2>;
+// }
 
 //extract - const res = await fetch(`/api/details/${params.id}`);
 //make it into a function-  const json = await res.json();
